c3f6559a216e7a8953bfdf85f1a27cbe7418a7d4
FurkanCRK <fcyazilim35@gmail.com>
2023-05-16 23:02:37 +0300

    oldu?

--- src/gitHub_ilkDers.java
+++ src/gitHub_ilkDers.java
@@ -2,5 +2,6 @@ public class gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello Gİthub");
         System.out.println("=============");
+        System.out.println("oldu mu?");
     }
 }
