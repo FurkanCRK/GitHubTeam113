c14b2b79a3fa4141bdb2b7e7690ed0ad5343cbb8
FurkanCRK <fcyazilim35@gmail.com>
2023-05-18 22:38:42 +0300

    IkinciGun2

--- src/C01_gitHub_ilkDers.java
+++ src/C01_gitHub_ilkDers.java
@@ -1,7 +1,9 @@
-public class gitHub_ilkDers {
+public class C01_gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello Gİthub");
         System.out.println("=============");
         System.out.println("oldu mu?");
+
+        System.out.println("merhaba ben frkn");
     }
 }
--- src/C02_gitHub_ilkDers.java
+++ src/C02_gitHub_ilkDers.java
@@ -1,2 +1,5 @@
 public class C02_gitHub_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("Merhaba gitHub ben geldim");
+    }
 }
